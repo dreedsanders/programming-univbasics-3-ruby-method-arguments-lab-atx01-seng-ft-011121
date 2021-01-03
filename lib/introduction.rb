85ba6a612ff5757efe20850c789c01df8c16cc09
kooky-range-8610 <dreedsanders@gmail.com>
2021-01-03 21:27:45 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,21 +2,21 @@ def introduction (name)
   puts "Hi, my name is #{name}." 
 end
 
-introduction "name"
+introduction "Donovan"
 
 
 def introduction_with_language (name , language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language "name", "language"
+introduction_with_language "Donovan", "Ruby"
 
 
 def introduction_with_language_optional (name , language = "Ruby")
-puts "Hi, my name is #{name} and I am learing to program in #{language}."
+puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language_optional "name"
+introduction_with_language_optional "Donovan" , "Lynux"
 
 
 
